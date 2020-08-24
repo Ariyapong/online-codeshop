3d1b4e92e43262f90ee87bb8625f5bb53e31e99d
Ariyapong <ariyapongw.ton@gmail.com>
2020-08-24 15:57:25 +0700

    inittial commit6

--- server/index.js
+++ server/index.js
@@ -18,7 +18,12 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 // Implement middleware
 app.use(cors());
-app.use(helmet());
+// app.use(helmet());
+app.use(
+  helmet({
+    contentSecurityPolicy: false,
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -29,10 +34,10 @@ app.use("/", codeRegisRouter);
 app.use("/api/reward", rewardRouter);
 app.use("/users", usersRouter);
 
-if (ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+if (ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
   app.use((req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
   });
 }
 
