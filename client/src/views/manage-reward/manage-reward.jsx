a37afa6269bd7d55e29dfef05c36646b2c08cb6d
Ariyapong <ariyapongw.ton@gmail.com>
2020-08-24 17:44:32 +0700

    init 1

--- client/src/views/manage-reward/manage-reward.jsx
+++ client/src/views/manage-reward/manage-reward.jsx
@@ -190,7 +190,7 @@ function ManageReward() {
 
   return (
     <div className={styles.submitFormLink}>
-      <p className="font-dy5">Reward RegistrationTT</p>
+      <p className="font-dy5">Reward Registration HUB</p>
       <form
         className={`${classes.root} customForm`}
         noValidate
