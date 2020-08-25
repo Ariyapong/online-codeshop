cf58c7748bb01d636695e01675278ab995093411
Ariyapong <ariyapongw.ton@gmail.com>
2020-08-25 14:41:10 +0700

    deploy 2

--- client/src/views/manage-reward/manage-reward.jsx
+++ client/src/views/manage-reward/manage-reward.jsx
@@ -203,7 +203,7 @@ function ManageReward() {
 
   return (
     <div className={styles.submitFormLink}>
-      <p className="font-dy5">Registration Reward Hub</p>
+      <p className="font-dy5">Registration Rewardssss</p>
       <form
         className={`${classes.root} customForm`}
         noValidate
